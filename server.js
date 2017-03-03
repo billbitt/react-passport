9b55d07de39abaa6ffe991f47df7536e4e4b782f
bill bittner <bittner.w@gmail.com>
2017-03-03 01:01:37 -0800

    auth folders and files. signup passport strategy

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 // Load dependencies.
 const express = require("express");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const config = require("./config");
+
+// Connect to the database and load models.
+require("./server/models").connect(config.dbUri);
 
 // Define variable to hold express().
 const app = express();
 // Tell app to look for static files in the below directories .
 app.use(express.static("./server/static/"));
 app.use(express.static("./client/dist/"));
-// Tell the app to parse HTTP body mesages
+// Tell the app to parse HTTP body mesages.
 app.use(bodyParser.urlencoded({ extended: false}));
+// Pass the passport middleware.
+app.use(passport.initialize());
+
+// Load passport strategies.
+const localSignupStrategy = require("./server/passport/local-signup");
+const localLoginStrategy = require("./server/passport/local-login");
+passport.use("local-signup", localSignupStrategy);
+passport.use("local-login", localLoginStrategy);
+
+// pass the authentication checker middleware.
+const authCheckMiddleware = require("./server/middleware/auth-check");
+app.use("/api", authCheckMiddleware);
 
 // Routes.
 const authRoutes = require("./server/routes/auth");
+const apiToures = require("./server/routes/api");
 app.use("/auth", authRoutes);
+app.use("/api", apiRoutes);
 
 // Define the port. 
 const PORT = process.env.PORT || 3000;
