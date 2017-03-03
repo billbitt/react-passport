5bbda156d5d19961e429647e006b8c155a497a3d
bill bittner <bittner.w@gmail.com>
2017-03-03 15:03:49 -0800

    updated base component with conditional nav links

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from "react";
 import { Link, IndexLink } from "react-router";
+import Auth from "../modules/Auth";
 
 const Base = ({ children }) => {
     return (
@@ -11,14 +12,22 @@ const Base = ({ children }) => {
                     <IndexLink to="/">React App</IndexLink>
                 </div>
 
-                <div className="top-bar-right">
-                    <Link to="/login">Log in</Link>
-                    <Link to="/signup">Sign up</Link>
-                </div>
+                {/*conditional statement to decide which menu to render*/}
+                {Auth.isUserAuthenticated() ? (
+                    <div className="top-bar-right">
+                        <Link to="/logout">Log out</Link>
+                    </div>
+                ) : (
+                    <div className="top-bar-right">
+                        <Link to="/login">Log in</Link>
+                        <Link to="/signup">Sign up</Link>
+                    </div>
+                )}                
 
             </div>
 
-            ({ children })
+            {/*child components will be rendered here */}
+            { children }
 
         </div>
                 
